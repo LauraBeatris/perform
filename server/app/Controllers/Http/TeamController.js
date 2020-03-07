1cb33ea0b40f046c782acf6522cfc336cb4cb191
LauraBeatris <laurabeatriserafim@gmail.com>
2020-03-07 12:10:38 -0300

    first commit

---
+++ server/app/Controllers/Http/TeamController.js
@@ -0,0 +1,23 @@
+'use strict'
+
+class TeamController {
+  async index ({ request, response, view }) {
+  }
+
+  async store ({ request, response }) {
+  }
+
+  async show ({ params, request, response, view }) {
+  }
+
+  async edit ({ params, request, response, view }) {
+  }
+
+  async update ({ params, request, response }) {
+  }
+
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = TeamController
